1fa65c4dbfdd21f2b6cf2e0378769c82b65b36f6
adir hashta <adirkoko15@gmail.com>
2024-05-22 21:21:50 +0300

    Starting work on tests, folder unittests.

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,42 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for primitives.Point class
+ *
+ * @author Adir and Meir
+ */
+class PointTest {
+
+    /**
+     * Test method for {@link primitives.Point#subtract(primitives.Point)}.
+     */
+    @Test
+    void subtract() {
+    }
+
+    /**
+     * Test method for {@link primitives.Point#add(primitives.Vector)}.
+     */
+    @Test
+    void add() {
+    }
+
+
+    /**
+     * Test method for {@link primitives.Point#distanceSquared(Point)}.
+     */
+    @Test
+    void distanceSquared() {
+    }
+
+    /**
+     * Test method for {@link primitives.Point#distance(Point)}.
+     */
+    @Test
+    void distance() {
+    }
+}
