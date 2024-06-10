8fbde60fb12f9ae1e368863b1517127082e7fc7f
adirk <adirkoko15@gmail.com>
2024-06-10 13:30:56 +0300

    new tag PR03

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -16,9 +16,9 @@ public class Triangle extends Polygon {
     /**
      * Constructor for Triangle using three points.
      *
-     * @param p1 First vertex of the triangle
-     * @param p2 Second vertex of the triangle
-     * @param p3 Third vertex of the triangle
+     * @param p1 First vertex of the triangle.
+     * @param p2 Second vertex of the triangle.
+     * @param p3 Third vertex of the triangle.
      */
     public Triangle(Point p1, Point p2, Point p3) {
         super(p1, p2, p3);
